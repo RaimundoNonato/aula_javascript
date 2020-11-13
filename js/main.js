063ae6db4ee38bf099eee216f13ca641e63812d0
Raimundo Nonato Pimenta Filho <raimundo.nonato@alumni.usp.br>
2020-11-13 17:54:39 -0300

    Aula de JS concluida

---
+++ js/main.js
@@ -0,0 +1,83 @@
+
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado</b>";
+}
+
+/*
+function clicou(){
+    alert("Você clicou no botão!");
+}
+*/
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+console.log(soma(3, 4));
+*/
+
+/*
+// Data
+d = new Date;
+alert(d);
+alert(d.getDay());
+*/
+
+/*
+for (let count = 0; count <= 5; count++) {
+    console.log(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+}
+*/
+
+/*
+var fruta = {nome: "maça", cor: "vermelha"};
+var frutas = [];
+console.log(fruta);
+console.log(frutas);
+frutas.push(fruta);
+console.log(frutas);
+//O que está abaixo modifica também o elemento que está na lista.
+fruta.nome = "uva";
+fruta.cor = "roxa";
+frutas.push(fruta);
+console.log(frutas);
+fruta.cor = "vermelha";
+*/
+
+//var lista = ["maçã","pêra","laranja"];
+//console.log(lista);
+//lista.push("uva");
+//console.log(lista);
+//lista.pop();
+//console.log(lista);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join("|"));
+
+//var nome = "Raimundo Nonato Pimenta Filho";
+//console.log(nome);
+
+//var n1 = 3;
+//var n2 = 4;
+//console.log(n1 + n2);
+
+//var frase = "Japão é o melhor time do mundo";
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
